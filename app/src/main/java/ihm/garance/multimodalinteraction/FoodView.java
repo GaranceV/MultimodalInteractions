df42dfcf97087d2dd6cd0a288ffc6787f9d8570a
Garance <garance.vallat@gmail.com>
2015-11-05 09:07:28 +0100

    added food and view

---
+++ app/src/main/java/ihm/garance/multimodalinteraction/FoodView.java
@@ -0,0 +1,31 @@
+package ihm.garance.multimodalinteraction;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+import ihm.garance.multimodalinteraction.images.Food;
+
+/**
+ * Created by Garance on 05/11/2015.
+ */
+public class FoodView extends View {
+    public Food toSort;
+
+    public FoodView(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+        toSort = new Food();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Bitmap myBitmap = BitmapFactory.decodeResource(getResources(), toSort.getPath());
+        canvas.drawBitmap(myBitmap, 0, 0, null);
+    }
+
+
+}
--- app/src/main/java/ihm/garance/multimodalinteraction/images/Food.java
+++ app/src/main/java/ihm/garance/multimodalinteraction/images/Food.java
@@ -1,7 +1,32 @@
 package ihm.garance.multimodalinteraction.images;
 
+import ihm.garance.multimodalinteraction.R;
+
 /**
  * Created by Garance on 05/11/2015.
  */
 public class Food {
+    private String name;
+    private int path;
+
+    public Food() {
+        name = "farine";
+        path = R.drawable.farine;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPath() {
+        return path;
+    }
+
+    public void setPath(int path) {
+        this.path = path;
+    }
 }
