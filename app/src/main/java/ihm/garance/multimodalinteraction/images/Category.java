a12df06248f18b2f285617063115f9b5ad71a048
SoniaTual <sonia.tual@orange.fr>
2015-11-05 08:35:03 +0100

    Création  class category

--- app/src/main/java/ihm/garance/multimodalinteraction/images/Category.java
+++ app/src/main/java/ihm/garance/multimodalinteraction/images/Category.java
@@ -4,4 +4,27 @@ package ihm.garance.multimodalinteraction.images;
  * Created by Garance on 05/11/2015.
  */
 public class Category {
+    private String name;
+    private String path;
+
+    public void Category(String name, String path){
+        this.name=name;
+        this.path=path;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 }
